9453053b5a1affdfe1bb3d2faaed84d2de581396
jaysaberola <wsi.jayanthonysaberola@gmail.com>
2025-09-03 16:10:33 +0800

    footer_update

--- resources/views/theme/layouts/footer.blade.php
+++ resources/views/theme/layouts/footer.blade.php
@@ -1,85 +1,134 @@
 <!-- Footer ============================================= -->
-<footer id="footer" class="bg-white">
+<footer id="footer" class="text-orange-500 py-12 mt-8 relative" style="background-image: url('{{ asset('images/landing-2/images/footer2.svg') }}'); background-size: cover; background-position: center; transform: scaleX(-1);">
+    <div class="absolute inset-0 bg-white-100/100"></div> <!-- Overlay for readability -->
+    <div class="container mx-auto px-4 relative z-10" style="transform: scaleX(-1);"> <!-- Counter-transform for content -->
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+            <!-- Company Info & Social Media -->
+            <div class="space-y-4">
+                <img src="images/landing-2/images/1679411331_wsi-logo.svg" alt="WebFocus Logo" class="h-16">
+                <p>We believe in Simple, Creative, Premium Quality & Flexible Design Standards.</p>
+                <h4 class="text-lg font-semibold">Follow Us</h4>
+                <div class="flex space-x-4">
+                    <a href="#" class="text-orange-500 hover:text-orange-300 transition">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="text-orange-500 hover:text-orange-300 transition">
+                        <i class="fa-brands fa-delicious"></i>
+                    </a>
+                    <a href="#" class="text-orange-500 hover:text-orange-300 transition">
+                        <i class="fa-brands fa-paypal"></i>
+                    </a>
+                </div>
+                <div class="flex space-x-2">
+                    <img src="images/landing-2/images/website-admired.png" alt="Website Admired Logo" class="h-25">
+                </div>
+                <div class="flex space-x-4">
+                    <h4 class="text-lg font-semibold">2018 ADMIRED BRAND</h4>
+                </div>
+                <p>FOR WEB DESIGN & DEVELOPMENT, IT. SOLUTIONS CATEGORY</p>
+                <p></p>
+                <img src="images/landing-2/images/sitelock.jpg" alt="SiteLock Logo" class="h-22">
+            </div>
 
-    <div class="container">
+            <!-- Contact Information -->
+            <div class="space-y-6">
+                <h4 class="text-lg font-semibold">Contact Us</h4>
+                <div class="space-y-6">
+                    <div>
+                        <p class="font-semibold">Our Office</p>
+                        <p>Unit 907-909 Antel Global Corporate Center, Julia Vargas Avenue, Ortigas Center, Pasig City, Philippines</p>
+                    </div>
+                    <div>
+                        <h4 class="font-semibold">Telephone</h4>
+                        <ul class="list-none space-y-2">
+                            <li>+63 (2) 8706-6144</li>
+                            <li>+63 (2) 8706-5796</li>
+                            <li>+63 (2) 8511-0528</li>
+                            <li>+63 (2) 8709-8061</li>
+                            <li>+63 (2) 8806-5201</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="font-semibold">Mobile</h4>
+                        <ul class="list-none space-y-2">
+                            <li>+63 908 869 4069 (Smart)</li>
+                            <li>+63 917 569 7380 (Globe)</li>
+                            <li>+63 922 330 8373 (Sun Cellular)</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="font-semibold">E-mail</h4>
+                        <ul class="list-none space-y-2">
+                            <li>Sales: <a href="mailto:sales@webfocus.ph" class="text-black-300 hover:underline">sales@webfocus.ph</a></li>
+                            <li>Marketing: <a href="mailto:marketing@webfocus.ph" class="text-black-300 hover:underline">marketing@webfocus.ph</a></li>
+                            <li>Billing: <a href="mailto:billing@webfocus.ph" class="text-black-300 hover:underline">billing@webfocus.ph</a></li>
+                            <li>Customer Care: <a href="mailto:customercare@webfocus.ph" class="text-black-300 hover:underline">customercare@webfocus.ph</a></li>
+                            <li>Tech Support: <a href="mailto:support@webfocus.ph" class="text-black-300 hover:underline">support@webfocus.ph</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-      <footer id="footer" class="bg-white">
+            <!-- About Us & Services -->
+            <div class="space-y-4">
+                <h4 class="text-lg font-semibold">About Us</h4>
+                <ul class="list-none space-y-2">
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">History</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Mission & Vision</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Our Company</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Message from the CEO</a></li>
+                </ul>
+                <h4 class="text-lg font-semibold">Hosting</h4>
+                <ul class="list-none space-y-2">
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Shared Cloud Hosting</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Dedicated Cloud Hosting</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Dedicated Bare-Metal Hosting</a></li>
+                </ul>
+            </div>
 
-			<div class="container">
-				<div class="footer-widgets-wrap">
+            <!-- Domains & Development -->
+            <div class="space-y-4">
+                <h4 class="text-lg font-semibold">Domain</h4>
+                <ul class="list-none space-y-2">
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Top-Level Domain</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Hybrid Top-Level Domain</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Country-Level Domain</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Education Domain</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Government Domain</a></li>
+                </ul>
+                <h4 class="text-lg font-semibold">Web Development</h4>
+                <ul class="list-none space-y-2">
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Web Design & Development</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">E-Commerce Solutions</a></li>
+                </ul>
+                <h4 class="text-lg font-semibold">Document Management System</h4>
+                <ul class="list-none space-y-2">
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">FileHold</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Docukit</a></li>
+                </ul>
+                <h4 class="text-lg font-semibold">Terms</h4>
+                <ul class="list-none space-y-2">
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Careers</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">News</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Contact Us</a></li>
+                    <li><a href="#" class="text-orange-500 hover:text-orange-300 transition">Privacy Policy</a></li>
+                </ul>
+            </div>
+        </div>
 
-					<div class="row col-mb-50">
+        <!-- Copyrights -->
+        <div class="text-center border-t border-orange-200 pt-3">
+            <h4>© WebFocus Solutions, Inc. | 2019 All Rights Reserved</h4>
+        </div>
+    </div>
 
-						<div class="col-md-4">
-							<div class="widget">
+    <!-- Font Awesome CDN (required for social icons) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-								<img src="images/landing-2/images/logo-webfocus.png" alt="footer webfocus Logo" class="py-3" style="height: 62px;">
+    <!-- Tailwind CSS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
 
-								<p>We believe in Simple, Creative, Premium Quality  &amp; Flexible Design Standards with.</p>
-
-								<div>
-									<a href="#" class="social-icon border-color h-border-transparent h-bg-facebook">
-										<i class="fa-brands fa-facebook-f"></i>
-										<i class="fa-brands fa-facebook-f"></i>
-									</a>
-
-									<a href="#" class="social-icon border-color h-border-transparent h-bg-delicious">
-										<i class="fa-brands fa-delicious"></i>
-										<i class="fa-brands fa-delicious"></i>
-									</a>
-
-									<a href="#" class="social-icon border-color h-border-transparent h-bg-paypal">
-										<i class="fa-brands fa-paypal"></i>
-										<i class="fa-brands fa-paypal"></i>
-									</a>
-								</div>
-
-							</div>
-						</div>
-						<div class="col-6 col-md-2 ms-md-auto">
-							<div class="widget">
-
-								<h4 class="">Product</h4>
-
-								<ul class="widget-li-noicon widget_links ms-0">
-									<li><a href="#">About Us</a></li>
-									<li><a href="#">Help & Support</a></li>
-									<li><a href="#">Pricing</a></li>
-									<li><a href="#">Purchase</a></li>
-								</ul>
-
-							</div>
-						</div>
-						<div class="col-6 col-md-2">
-							<div class="widget">
-
-								<h4 class="">Terms</h4>
-
-								<ul class="widget-li-noicon widget_links ms-0">
-									<li><a href="#">Careers</a></li>
-									<li><a href="#">Privacy &amp; Policy</a></li>
-									<li><a href="#">Licence</a></li>
-									<li><a href="#">Contact</a></li>
-								</ul>
-
-							</div>
-						</div>
-					</div>
-
-				</div><!-- .footer-widgets-wrap end -->
-
-			</div>
-
-		</footer><!-- #footer end -->
-
-
-
-<!-- Font Awesome CDN (required for social icons) -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-papP+..." crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-<!-- Footer JS -->
-<script src="js/plugins.min.js"></script>
-<script src="js/functions.bundle.js"></script>
-
-
-</script>
+    <!-- Footer JS -->
+    <script src="js/plugins.min.js"></script>
+    <script src="js/functions.bundle.js"></script>
+</footer>
